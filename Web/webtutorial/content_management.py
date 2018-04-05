7a2fdd9e23ab23be257b3f043fd16ee248d746c9
keran0107 <kl2993@columbia.edu>
2018-04-05 15:12:17 -0400

    webdesign

---
+++ Web/webtutorial/content_management.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 20:15:55 2018
+
+@author: Keran
+"""
+
+
+def content():
+    TOPIC_DICT={"Basics":[["plot1","/plot1/"],
+                        ["plot2","/plot2/"],
+                        ["plot3","/plot3/"]],
+                "Web Dev":[["winesearch","/winesearch/"]]}
+    return TOPIC_DICT
+    
